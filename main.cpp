fdeba5ef1d9d594255c49ebcce4816bd0d62f38b
Marina_Mirs <125971894+Miraiel@users.noreply.github.com>
2023-03-02 11:13:51 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,160 @@
+﻿#include<iostream>
+using namespace std;
+
+class String;
+String operator+(const String& left, const String& right);
+
+class String
+{
+	int size;	//размер строки в Байтах
+	char* str;	//указатель на строку в динамической памяти
+public:
+	int get_size()const
+	{
+		return size;
+	}
+	const char* get_str()const
+	{
+		return str;
+	}
+	char* get_str()
+	{
+		return str;
+	}
+	
+	//-------------------Contructors------------------------
+	explicit String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "DefConstructor:\t" << this << endl;
+	}
+	String(const char* str)
+	{
+		this->size = strlen(str) + 1;//+1 для NULL-terminator
+		this->str = new char[size] {};
+		for (int i = 0; str[i]; i++)this->str[i] = str[i];
+		cout << "Constructor:\t" << this << endl;
+	}
+	//Deep copy (Побитовое копирование)
+	//other
+	//this
+	//Shallow copy (Поверхностное копирование)
+	String(const String& other)
+	{
+		this->size = other.size;
+		//Deep copy (Побитовое копирование):
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)
+			this->str[i] = other.str[i];
+		//-----------------------------
+		cout << "CopyConstructor:" << this << endl;
+	}
+
+	String(String&& other)noexcept //конструктор перемещения, сам конструктор должен переносит с-строку с other на this, делая при этом other пустым
+	{
+		size = other.size;
+		str = other.str;
+		other.size = 0;
+		other.str = nullptr;
+	}
+
+	~String()
+	{
+		delete[] this->str;
+		this->str = nullptr;
+		cout << "Destructor:\t" << this << endl;
+		//Debug assertion failed
+	}
+
+	//------------------Operators-------------------
+	String& operator=(const String& other)
+	{
+		/*int a = 2;
+		int b = 3;
+		a = b;*/
+
+		if (this == &other)return *this;
+		delete[] this->str;
+
+		this->size = other.size;
+		//Deep copy (Побитовое копирование):
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)
+			this->str[i] = other.str[i];
+		//-----------------------------
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
+	}
+	String& operator+=(const String& other)
+	{
+		return *this = *this + other;
+	}
+
+	char operator[](int i)const
+	{
+		return str[i];
+	}
+	char& operator[](int i)
+	{
+		return str[i];
+	}
+
+	//-----------------Methods--------------------------
+	void print()const
+	{
+		cout << "Size:\t" << size << endl;
+		cout << "Str:\t" << str << endl;
+	}
+};
+
+String operator+(const String& left, const String& right)
+{
+	String cat(left.get_size() + right.get_size() - 1);
+	for (int i = 0; i < left.get_size(); i++)
+		cat[i] = left[i];
+	//cat.get_str()[i] = left.get_str()[i];
+	for (int i = 0; i < right.get_size(); i++)
+		cat[i + left.get_size() - 1] = right[i];
+	//cat.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+	return cat;
+
+	//		l-value = r-value;
+}
+
+std::ostream& operator<<(std::ostream& os, const String& obj)
+{
+	return os << obj.get_str();
+}
+
+#define HOME_WORK
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	/*String str;
+	str.print();*/
+
+#ifdef HOME_WORK
+	String str1 = "Hello";
+	str1 = str1;
+	cout << str1 << endl;
+
+	String str2 = "World";
+	cout << str2 << endl;
+
+	//String str3 = str1 + str2;	//Copy constructor
+	String str3;
+	str3 = str1 + " " + str2;	//Copy assignment
+	cout << str3 << endl;
+
+	str1 += str2;
+	cout << str1 << endl;
+#endif // HOME_WORK
+
+	//MoveMethods:
+	//"The rule of three": CopyConstructor, CopyAssignment, ~Destructor
+	//"The rule of zero"
+	//C++ 11
+	//"The rule of five":  MoveConstructor, MoveAssignment
+}
