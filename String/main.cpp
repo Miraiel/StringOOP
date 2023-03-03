15bc94e3235314588456f457d48ebb8171d3d21b
Марина <@MARINA>
2023-03-03 19:43:38 +0300

    Constructors Delegation DONE

--- String/main.cpp
+++ String/main.cpp
@@ -29,7 +29,7 @@ public:
 		//this->str = new char[size] {};
 		cout << "DefConstructor:\t" << this << endl;
 	}
-	String(const char* str): size(strlen(str) + 1), str(new char[size] {})
+	String(const char* str): String(strlen(str)+1) //size(strlen(str) + 1), str(new char[size] {})
 	{
 		//this->size = strlen(str) + 1;//+1 для NULL-terminator
 		//this->str = new char[size] {};
@@ -40,13 +40,13 @@ public:
 	//other
 	//this
 	//Shallow copy (Поверхностное копирование)
-	String(const String& other):size(other.size), str(new char[size]{})
+	String(const String& other):String(other.str)	//size(other.size), str(new char[size]{})
 	{
 		//this->size = other.size;
 		//Deep copy (Побитовое копирование):
 		//this->str = new char[size] {};
-		for (int i = 0; i < size; i++)
-			this->str[i] = other.str[i];
+		//for (int i = 0; i < size; i++)
+		//	this->str[i] = other.str[i];
 		//-----------------------------
 		cout << "CopyConstructor:" << this << endl;
 	}
@@ -144,7 +144,6 @@ std::ostream& operator<<(std::ostream& os, const String& obj)
 
 #define HOME_WORK
 
-
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -161,7 +160,7 @@ void main()
 
 	//String str3 = str1 + str2;	//Copy constructor
 	String str3;
-	str3 = str1 + " " + str2;	//Copy assignment
+	str3 = str1 + str2;	//Copy assignment
 	cout << str3 << endl;
 
 	str1 += str2;
